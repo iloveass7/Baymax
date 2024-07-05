90cb5b1d50b6dee05a7e1ef339ae2b0e3ae98043
alimbinyeasin <alimbinyeasin@gmail.com>
2024-07-05 23:21:09 +0600

    signup,login

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { getSingleUser, getallUser, updateUser, deleteUser } from "../Controllers/userController.js";
-import { authenticate,restrict } from '../auth/verifyToken.js';
+import { getSingleUser, getallUser, updateUser, deleteUser, getUserProfile, getMyAppointments } from "../Controllers/userController.js";
+import { authenticate, restrict } from '../auth/verifyToken.js';
 
 
 const router = express.Router();
 
-router.get('/:id',authenticate,restrict(["patient"]),getSingleUser);
-router.get('/',authenticate,restrict(["admin"]), getallUser);
-router.put('/:id',authenticate,restrict(["patient"]), updateUser);
-router.delete('/:id',authenticate,restrict(["patient"]), deleteUser);
+router.get('/:id', authenticate, restrict(["patient"]), getSingleUser);
+router.get('/', authenticate, restrict(["admin"]), getallUser);
+router.put('/:id', authenticate, restrict(["patient"]), updateUser);
+router.delete('/:id', authenticate, restrict(["patient"]), deleteUser);
+router.get('/profile/me', authenticate, restrict(["patient"]), getUserProfile);
+router.get('/appointments/my-appointments', authenticate, restrict(["patient"]), getMyAppointments);
 
 export default router;
