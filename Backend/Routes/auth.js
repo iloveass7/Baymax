1bae5737f6935d6e3ffa199f3b7f5d12d4b444f7
iloveass7 <borshonjsr@gmail.com>
2024-07-07 03:07:32 +0600

    FIx

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,10 +1,11 @@
 
 import express from "express";
-import { register, login } from "../Controllers/authController.js";
+import { register, login, test } from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get('/test', test);
 
 export default router;
--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://baymax-api.vercel.app/";
+export const BASE_URL = "http://localhost:5000";
 export const token = localStorage.getItem("token");
