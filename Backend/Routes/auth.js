57440ea882bf22b05a01596695a801f7e55f48aa
Alim Bin Yeasin <111185275+alim-55@users.noreply.github.com>
2024-07-07 04:53:53 +0600

    Update auth.js

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,11 +1,10 @@
 
 import express from "express";
-import { register, login, test } from "../Controllers/authController.js";
+import { register, login } from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get('/test', test);
 
 export default router;
