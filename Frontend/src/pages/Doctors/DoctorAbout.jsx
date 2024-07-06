cfdc3cef031ded0c1342c2bb3517cb38d6e741cd
iloveass7 <borshonjsr@gmail.com>
2024-07-06 22:11:13 +0600

    doctorprofile+edit

--- Frontend/src/pages/Doctors/DoctorAbout.jsx
+++ Frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -2,20 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { formatDate } from "../../utils/formatDate";
 
-const DoctorAbout = () => {
+const DoctorAbout = ({ name, about, qualifications, experiences }) => {
   return (
     <div>
       <div>
         <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2 ">
           About of
           <span className="text-irisBlueColor font-bold text-[24px] leading-9  ">
-            Alim Bin Yeasin
+            {name}
           </span>
         </h3>
         <p className="text_para">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde itaque
-          laudantium est totam illo, commodi temporibus maiores explicabo
-          reiciendis veniam.
+          {about}
         </p>
       </div>
 
@@ -23,35 +21,23 @@ const DoctorAbout = () => {
         <h3 className="text-20px] leading-[30px] text-headingColor font-semibold ">
           Education
         </h3>
+
         <ul className="pt-4 md:p-5">
-          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]  ">
+          {qualifications?.map((item, index) => <li key={index} className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]  ">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold ">
-                {formatDate("10-22-2010")}
+                {formatDate(item.startingDate)} - {formatDate(item.endingDate)}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
-                PHD in Surgeon
+                {item.degree}
               </p>
             </div>
             <p className="text-[14px] leading-5 font-medium text-textColor flex items-center">
               <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-              Square Hospital, Dhaka.
+              {item.university}
             </p>
-          </li>
-          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]  ">
-            <div>
-              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold ">
-                {formatDate("10-22-2008")}
-              </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
-                PHD in Medicine
-              </p>
-            </div>
-            <p className="text-[14px] leading-5 font-medium text-textColor flex items-center">
-              <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-              Square Hospital, Dhaka.
-            </p>
-          </li>
+          </li>)}
+
         </ul>
       </div>
 
@@ -61,33 +47,19 @@ const DoctorAbout = () => {
         </h3>
 
         <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5  ">
-          <li className="p-4 rounded bg-[#fff9ea]">
+          {experiences?.map((item,index)=> <li key={index} className="p-4 rounded bg-[#fff9ea]">
             <span className="text-yellowColor text-[15px] leading-6 font-semibold">
-              {formatDate("07-04-2010")} - {formatDate("08-13-2014")}
+              {formatDate(item.startingDate)} - {formatDate(item.endingDate)} 
             </span>
             <p className="text-[16px] leading-6 font-medium text-textColor">
-              Sr. Surgeon
+              {item.position}
             </p>
             <p className="text-[14px] leading-5 font-medium text-textColor flex items-center">
               <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-              Square Hospital, Dhaka.
+              {item.hospital}
             </p>
-          </li>
-          <li className="p-4 rounded bg-[#fff9ea]">
-            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
-              {formatDate("07-04-2010")} - {formatDate("08-13-2014")}
-            </span>
-            <p className="text-[16px] leading-6 font-medium text-textColor">
-              Sr. Surgeon
-            </p>
-            <p className="text-[14px] leading-5 font-medium text-textColor flex items-center">
-              <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-              Square Hospital, Dhaka.
-            </p>
-          </li>
-          <li className="p-4 rounded bg-[#fff9ea]  ">
-
-          </li>
+          </li>)}
+          
         </ul>
       </div>
     </div>
