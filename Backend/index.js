530efaeeb760e5968ef3b9b52e81526a90a2c001
Alim Bin Yeasin <111185275+alim-55@users.noreply.github.com>
2024-09-21 20:31:12 +0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const corsOptions = {
-    origin: ["https://baymax-fawn.vercel.app"],
+    origin: ["true"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 };
